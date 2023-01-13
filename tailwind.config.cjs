17fee36dabe6db0125e46c65b369b7d16d1b32f4
Cesar Funes <fuunesto@gmail.com>
2023-01-12 19:46:50 -0600

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [{
+      "catppuccin-mocha": {
+        primary: "#89b4fa", // blue
+        secondary: "#f5c2e7", // pink
+        accent: "#94e2d5", // teal
+        neutral: "#11111b", // crust
+        "base-100": "#1e1e2e", // base
+        info: "#74c7ec", // sapphire
+        success: "#a6e3a1", // green
+        warning: "#f9e2af", // yellow
+        error: "#f38ba8", // red
+      },
+    }],
+  },
+};
