4740d0100308c8db1f95102baa2261cb6ce8e3e8
Cesar Funes <fuunesto@gmail.com>
2023-03-07 21:09:21 -0600

    layout and alert created

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,18 +7,19 @@ module.exports = {
   plugins: [require("daisyui")],
   daisyui: {
     logs: false,
-    themes: [{
-      "catppuccin-mocha": {
-        primary: "#89b4fa", // blue
-        secondary: "#f5c2e7", // pink
-        accent: "#94e2d5", // teal
-        neutral: "#11111b", // crust
-        "base-100": "#1e1e2e", // base
-        info: "#74c7ec", // sapphire
-        success: "#a6e3a1", // green
-        warning: "#f9e2af", // yellow
-        error: "#f38ba8", // red
-      },
-    }],
+    themes: ["dracula"],
+    // themes: [{
+    //   "catppuccin-mocha": {
+    //     primary: "#89b4fa", // blue
+    //     secondary: "#f5c2e7", // pink
+    //     accent: "#94e2d5", // teal
+    //     neutral: "#11111b", // crust
+    //     "base-100": "#1e1e2e", // base
+    //     info: "#74c7ec", // sapphire
+    //     success: "#a6e3a1", // green
+    //     warning: "#f9e2af", // yellow
+    //     error: "#f38ba8", // red
+    //   },
+    // }],
   },
 };
